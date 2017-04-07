a1957e9b36a5f29ca6d2de5c1fbe4afe5639d0bc
root <root@ip-10-120-238-114.ec2.internal>
2017-04-07 06:43:54 +0000

    Updating script

--- Remote-linux-host.py
+++ Remote-linux-host.py
@@ -2,7 +2,7 @@ import subprocess
 from subprocess import Popen, PIPE
 
 
-#ret = subprocess.call(["ssh", "-i", "pass.pem", "root@75.101.199.181", "hostname && uptime"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
-ret = subprocess.Popen(["ssh", "-i", "pass.pem", "root@75.101.199.181", "hostname && upptime"], stdout=subprocess.PIPE, stderr=subprocess.PIPE);
+status = subprocess.call(["ssh", "-i", "pass.pem", "root@IP", "hostname && uptime"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
+ret = subprocess.Popen(["ssh", "-i", "keypass.pem", "root@IP", "hostname && upptime"], stdout=subprocess.PIPE, stderr=subprocess.PIPE);
 print ret.stdout.read()
 print ret.stderr.read()
